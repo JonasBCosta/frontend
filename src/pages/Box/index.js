44012cae6c3da134c20cffdd33cb0899170d3f57
JonasBCosta <48065552+JonasBCosta@users.noreply.github.com>
2019-04-22 23:47:34 -0300

    lixo

--- src/pages/Box/index.js
+++ src/pages/Box/index.js
@@ -23,10 +23,8 @@ export default class Box extends Component {
         const box = this.props.match.params.id; // Pega o ID do box
         const io  = socket("https://omnistack-backend9.herokuapp.com"); // Abre conexão
         io.emit("connectRoom", box); //Envia uma mensagem. Lá no back-end, "connectRoom é é a mensagem de conexão do usuário em uma box". Agora este socket ouvirá todas as mensagens dessa box
-        io.on("file", data => { // file é o tipo do arquivo, e data é o arquivo 
-            // Para adicionar um arquivo no state, é necessário pegar o box inteiro, copiar tudo de novo o que ele já tem com a adição do arquivo novo
-            // Segue-se o conceito de imutabilidade, que não altera a informação mas cria uma nova informação
-            this.setState({ box: { ... this.state.box, files: [data, ... this.state.box.files]} }); // Coloca o data no inicio da fila
+        io.on("file", data => { 
+            console.log(data);
         });
     }
 
